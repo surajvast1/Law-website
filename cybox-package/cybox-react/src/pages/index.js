9a512e113bb27d9b242ca135412b2774c7aefee6
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2023-04-19 22:23:40 +0530

    Update index.js

--- cybox-package/cybox-react/src/pages/index.js
+++ cybox-package/cybox-react/src/pages/index.js
@@ -15,27 +15,28 @@ import Blog from "./Blog";
 import BlogDetails from "./BlogDetails";
 import Contact from "./Contact";
 import TeamDetail from "./TeamDetail";
-
+import Service from "./Service";
+import "./service.css";
 
 const routes = [
-  { path: '/', component: <HomeOne />},
-  { path: '/home-v2', component: <HomeTwo />},
-  { path: '/home-v3', component: <HomeThree />},
-  { path: '/about-v1', component: <AboutOne />},
-  { path: '/about-v2', component: <AboutTwo />},
-  { path: '/coming-soon', component: <ComingSoon />},
-  { path: '/community', component: <Community />},
-  { path: '/faq', component: <FAQ />},
-  { path: '/404', component: <Page404 />},
-  { path: '/road-map-v1', component: <RoadMapOne />},
-  { path: '/road-map-v2', component: <RoadMapTwo />},
-  { path: '/nft-item', component: <NftItem />},
-  { path: '/nft-item-details', component: <NftItemDetails />},
-  { path: '/blog', component: <Blog />},
-  { path: '/blog-details', component: <BlogDetails />},
-  { path: '/contact', component: <Contact />},
-  { path: '/team-details', component: <TeamDetail />},
-
-]
+  { path: "/", component: <HomeOne /> },
+  { path: "/home-v2", component: <HomeTwo /> },
+  { path: "/home-v3", component: <HomeThree /> },
+  { path: "/about-v1", component: <AboutOne /> },
+  { path: "/about-v2", component: <AboutTwo /> },
+  { path: "/coming-soon", component: <ComingSoon /> },
+  { path: "/community", component: <Community /> },
+  { path: "/faq", component: <FAQ /> },
+  { path: "/404", component: <Page404 /> },
+  { path: "/road-map-v1", component: <RoadMapOne /> },
+  { path: "/road-map-v2", component: <RoadMapTwo /> },
+  { path: "/nft-item", component: <NftItem /> },
+  { path: "/nft-item-details", component: <NftItemDetails /> },
+  { path: "/blog", component: <Blog /> },
+  { path: "/blog-details", component: <BlogDetails /> },
+  { path: "/contact", component: <Contact /> },
+  { path: "/team-details", component: <TeamDetail /> },
+  { path: "/service", component: <Service /> },
+];
 
 export default routes;
