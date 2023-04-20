b7ffbb66d94d311a7def067363bbec8d6e844beb
Vyom-26 <128027346+Vyom-26@users.noreply.github.com>
2023-04-20 19:10:11 +0530

    Updated index.jsx
    Changed the E-CORP logo image on the navigation bar.

--- cybox-package/cybox-react/src/components/header/index.jsx
+++ cybox-package/cybox-react/src/components/header/index.jsx
@@ -5,7 +5,7 @@ import { Link , NavLink } from 'react-router-dom';
 import menus from '../../pages/menu';
 
 import './styles.scss';
-import logo from '../../assets/images/logo/logo.png'
+import logo from '../../assets/images/logo/logonew1.png'
 import Button from '../button';
 
 
