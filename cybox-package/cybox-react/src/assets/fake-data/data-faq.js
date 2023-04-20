761156115a5dba5e9bffde88a240eb8378416866
Vyom-26 <128027346+Vyom-26@users.noreply.github.com>
2023-04-20 19:24:48 +0530

    Updated data-faq.js
    Changed and updated the faq's page.

--- cybox-package/cybox-react/src/assets/fake-data/data-faq.js
+++ cybox-package/cybox-react/src/assets/fake-data/data-faq.js
@@ -1,34 +1,34 @@
 const dataFaq = [
     {
         id: 1,
-        title: 'What are the NFTs?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: 'What services does E-CORP offer?',
+        content: "E-CORP offers a range of services to assist with business compliance, including registrations, tax filings, bookkeeping, and other legal matters. Additionally, E-CORP offers assistance with pitch preparation and funding.",
         show: 'show'
     },
     {
         id: 2,
-        title: 'How do i get NFTs?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: "How does E-CORP's AI technology help with compliance?",
+        content: "E-CORP's AI technology ensures reliable and accurate calculations, eliminating any chance of errors in bookkeeping or tax return preparation. This helps to streamline the compliance process and save time and money in the long run.",
     },
     {
         id: 3,
-        title: 'How can we buy your NFTs?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: "Who can benefit from E-CORP's services?",
+        content: "E-CORP's services are primarily aimed at startups and entrepreneurs who need help with compliance requirements, but any business can benefit from E-CORP's services.",
     },
     {
         id: 4,
-        title: 'Who are the team behind the project?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: "How much does E-CORP's platform cost?",
+        content: "The cost of E-COPR's platform depends on the specific services that are needed. E-CORP offers various pricing plans to fit different business needs and budgets.",
     },
     {
         id: 5,
-        title: 'How can we buy your NFTs?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: "Is E-CORP's platform easy to use?",
+        content: "Yes, E-CORP's platform is designed to be user-friendly and easy to navigate, even for those who are not familiar with compliance requirements.",
     },
     {
         id: 6,
-        title: 'Who are the team behind the project?',
-        content: 'Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.',
+        title: "What sets E-CORP apart from other compliance platforms?",
+        content: "E-CORP's AI technology provides reliable and accurate calculations, and the platform offers a range of services to assist with compliance requirements, as well as assistance with pitch preparation and funding. Additionally, E-CORP's platform is designed to be user-friendly and easy to use.",
     },
 ]
 
