4ec9fa8c5b153cab65d48e35cfbe6acd49d9a19e
Shubham Srivastava <99933579+surajvast1@users.noreply.github.com>
2023-04-21 16:46:56 +0530

    changed app.js

--- cybox-package/cybox-react/src/App.js
+++ cybox-package/cybox-react/src/App.js
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import { React, useEffect } from "react";
-import AOS from "aos";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import "../src/assets/font/font-awesome.css";
-import routes from "./pages";
-// import Page404 from './pages/404';
-=======
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import { Route, Routes, useLocation } from 'react-router-dom';
@@ -16,7 +6,6 @@ import Footer from './components/footer';
 import '../src/assets/font/font-awesome.css';
 import routes from './pages';
 import Page404 from './pages/404';
->>>>>>> Stashed changes
 
 function App() {
   useEffect(() => {
@@ -25,20 +14,6 @@ function App() {
     });
   }, []);
 
-<<<<<<< Updated upstream
-  return (
-    <>
-      <Header />
-
-      <Routes>
-        {routes.map((data, idx) => (
-          <Route key={idx} path={data.path} element={data.component} exact />
-        ))}
-        {/* <Route path='*' element={<Page404 />} /> */}
-      </Routes>
-
-      <Footer />
-=======
   const location = useLocation();
   const isComingSoonPage = location.pathname !== '/coming-soon';
 
@@ -54,7 +29,6 @@ function App() {
       </Routes>
 
       {isComingSoonPage && <Footer />}
->>>>>>> Stashed changes
     </>
   );
 }
