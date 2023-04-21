fc27be7d33c281943f90d798ba7ffbaf0dec6eac
Shashank-9680 <shashankagrawal496@gmail.com>
2023-04-21 15:49:03 +0530

    this is a commit

--- cybox-package/cybox-react/src/App.js
+++ cybox-package/cybox-react/src/App.js
@@ -1,40 +1,33 @@
-
-import {React , useEffect} from 'react';
-import AOS from 'aos';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/header';
-import Footer from './components/footer';
-import '../src/assets/font/font-awesome.css'
-import routes from './pages';
-import Page404 from './pages/404';
+import { React, useEffect } from "react";
+import AOS from "aos";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import "../src/assets/font/font-awesome.css";
+import routes from "./pages";
+// import Page404 from './pages/404';
 
 function App() {
-    
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
 
-    useEffect(() => {
-        AOS.init({
-          duration : 2000
-        }); 
-      }, []);
+  return (
+    <>
+      <Header />
 
-    return (
-        <>
+      <Routes>
+        {routes.map((data, idx) => (
+          <Route key={idx} path={data.path} element={data.component} exact />
+        ))}
+        {/* <Route path='*' element={<Page404 />} /> */}
+      </Routes>
 
-            <Header />
-
-            <Routes>
-
-                {
-                    routes.map((data,idx) => (
-                        <Route key={idx} path={data.path} element={data.component} exact />
-                    ))
-                }
-                <Route path='*' element={<Page404 />} />
-            </Routes>
-
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 }
 
 export default App;
