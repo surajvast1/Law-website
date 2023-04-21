aa72dc68214fe9d4b2753e53152ac603ad777c3d
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2023-04-21 13:53:39 +0530

    Update Service.jsx

--- cybox-package/cybox-react/src/pages/Service.jsx
+++ cybox-package/cybox-react/src/pages/Service.jsx
@@ -5,30 +5,218 @@ const Service = () => {
   return (
     <div>
       <PageTitle title="" />
-      <div className="s-container">
-        <div className="left-service">
-          <h3>Our Services</h3>
-          <p>We can file your gst with the help of our AI model.</p>
-          <button className="s-button">
-            Home {">"}
-            {">"} Services
-          </button>
-        </div>
-        <div className="right-service">
-          <img
-            className="triangle-image"
-            src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/homeshape-bg.png"
-            alt=""
-          />
-          <div className="image-container">
+      <section className="Servives">
+        <div className="s-container">
+          <div className="left-service">
+            <h3>Our Services</h3>
+            <p>We can file your gst with the help of our AI model.</p>
+            <button className="s-button">
+              Home {">"}
+              {">"} Services
+            </button>
+          </div>
+          <div className="right-service">
             <img
-              className="hand-image"
-              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/CONTACT-US-HAND.png"
+              className="triangle-image"
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/homeshape-bg.png"
               alt=""
             />
+            <div className="image-container">
+              <img
+                className="hand-image"
+                src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/CONTACT-US-HAND.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+
+      <section className="s-we-provide">
+        <div className="s-provide-container">
+          <h6>Service We Provide</h6>
+          <h2>Our Purpose is To Deliver Excellence in Service and Execution</h2>
+        </div>
+
+        <div className="card-container">
+          {/* <div className="left-cards"> */}
+          <div className="main-card">
+            <div className="s-card">
+              {/* <div className="black-line"></div> */}
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/services-sec-home-page-.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Robotic Automation</h6>
+                <p>
+                  Quis autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/services-sec-home-page-1.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Machine learning</h6>
+                <p>
+                  Suir autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui .
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/services-sec-home-page-02.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Education & Science</h6>
+                <p>
+                  Tuis autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          {/* <div className="right-cards"> */}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/services-sec-home-page-3.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Predictive Analysis</h6>
+                <p>
+                  Muis autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/about-image-5.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Electronics & Technology</h6>
+                <p>
+                  Nuis autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/about-image-6.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Metal and Machining</h6>
+                <p>
+                  Guis autem vel eum iure reprehenderit rui in ea voluptate
+                  velit esse quam nihil molestia consequatur, vel illum rui.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+      </section>
+      <section className="s-information">
+        <div className="s-information-container">
+          <div className="left-s-info">
+            <img
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/home-why-choose-us-image.png"
+              alt=""
+            />
+            {/* <img
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/home-why-choose-us-bg-image.png"
+              alt=""
+            /> */}
+          </div>
+          <div className="right-s-info">
+            <h6>WHY CHOOSE US</h6>
+            <h2>Get Closer Look How Business Develop in AI Data Analysis</h2>
+
+            <p>
+              Consectetur adipiscing elit sed do eiusmod tempor in labore et
+              dolore magna aliqua ruis ipsum suspendisse ultrices gravida sit
+              amet.
+            </p>
+            <div className="quis-information">
+              <span>Quis autem vel eum iure reprehenderit aui</span>
+              <br />
+              <span>Quis autem vel eum iure reprehenderit aui</span>
+              <br />
+              <span>Quis autem vel eum iure reprehenderit aui</span>
+              <br />
+              <span>Quis autem vel eum iure reprehenderit aui</span>
+              <br />
+            </div>
+            <button className="READMORE">
+              {" "}
+              Read More
+              <div className="container-icon"></div>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
