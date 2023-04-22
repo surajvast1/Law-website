f88ec46ef6a30e9e2b7724f071eed59644e0dd8f
Vyom-26 <128027346+Vyom-26@users.noreply.github.com>
2023-04-22 12:31:40 +0530

    Updated Services.jsx
    Edited the entire Services page , removed some unwanted buttons and edited the content on the pages

--- cybox-package/cybox-react/src/pages/Service.jsx
+++ cybox-package/cybox-react/src/pages/Service.jsx
@@ -5,30 +5,213 @@ const Service = () => {
   return (
     <div>
       <PageTitle title="" />
-      <div className="s-container">
-        <div className="left-service">
-          <h3>Our Services</h3>
-          <p>We can file your gst with the help of our AI model.</p>
-          <button className="s-button">
-            Home {">"}
-            {">"} Services
-          </button>
-        </div>
-        <div className="right-service">
-          <img
-            className="triangle-image"
-            src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/homeshape-bg.png"
-            alt=""
-          />
-          <div className="image-container">
+      <section className="Servives">
+        <div className="s-container">
+          <div className="left-service">
+            <h3 className="Heading-Text">Our Services</h3>
+            <p className="Under-Heading-Text-Service-Page">We can file your gst with the help of our AI model.</p>
+            {/* <button className="s-button">
+              Home {">"}
+              {">"} Services
+            </button> */}
+          </div>
+          <div className="right-service">
             <img
-              className="hand-image"
-              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/CONTACT-US-HAND.png"
+              className="triangle-image"
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/ecorp-img.png"
               alt=""
             />
+            
           </div>
         </div>
-      </div>
+      </section>
+
+      <section className="s-we-provide">
+        <div className="s-provide-container">
+          <h6>Service We Provide</h6>
+          <h2>Our Purpose is To Deliver Excellence in Service and Execution</h2>
+        </div>
+
+        <div className="card-container">
+          {/* <div className="left-cards"> */}
+          <div className="main-card">
+            <div className="s-card">
+              {/* <div className="black-line"></div> */}
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/Pitch-300x300.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Incorporation</h6>
+                <p className="inner-text">
+                Want to open a company, but don't know where to start? Start incorporating your dream company here.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE <br /><br />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/services-sec-home-page-.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>GST Registration </h6>
+                <p className="inner-text">
+                We provide one-stop destination for all you GST filling needs.
+                
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE <br /><br /><br /><br/>
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/TM-300x297.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Trademark</h6>
+                <p className="inner-text">
+                Register your trademarks with us in a simple hazel free procedure.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE <br /><br /><br /><br />
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          {/* <div className="right-cards"> */}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/Pitch-300x300.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Pitch Deck</h6>
+                <p className="inner-text">
+                To effectively communicate your vision, strategy, and financial projections to potential investors, partners, and customers.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE<br /><br />
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/FUNDNG-300x300.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>Funding</h6>
+                <p className="inner-text">
+                Register your trademarks with us in a simple hazel free procedure.
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE <br /><br /><br /><br />
+                </a>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="main-card">
+            <div className="s-card">
+              <div className="left-part">
+                <div className="card-image">
+                  <img
+                    src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/about-image-6.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="right-part">
+                <h6>MORE SERVICES</h6>
+                <p className="inner-text">
+                ISO Certification <br />
+                Bookkeeping Services <br />
+                OPC Registration <br />
+                GST Registration <br />
+                IEC Registration <br />
+                LLP Registration <br />
+                </p>
+                <a className="read-more" href="#">
+                  READ MORE<br />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+      </section>
+      <section className="s-information">
+        <div className="s-information-container">
+          <div className="left-s-info">
+            <img
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/home-why-choose-us-image.png"
+              alt=""
+            />
+            {/* <img
+              src="https://ecorpwpsrv-0f9814189c3cdd15bdb8-endpoint.azureedge.net/blobecorpwpsrv0c6c233083/wp-content/uploads/2023/03/home-why-choose-us-bg-image.png"
+              alt=""
+            /> */}
+          </div>
+          <div className="right-s-info">
+            <h6 >WHY CHOOSE US</h6>
+            <h2>We are great at what we do </h2>
+
+            <p className="WHYCHOOSEUS-TEXT">
+            Here's some general content about our GST company:
+            </p>
+            <div className="quis-information">
+              <img src="https://w7.pngwing.com/pngs/133/147/png-transparent-blue-and-teal-arrow-logo-arrow-bullet-computer-icons-bullet-holes-angle-triangle-weapons.png" alt="" className="bluemark" />
+              <span>   They prioritize efficiency, accuracy, and exceptional customer service to help clients stay on top of their GST obligations.</span>
+              <br /><br />
+              <img src="https://w7.pngwing.com/pngs/133/147/png-transparent-blue-and-teal-arrow-logo-arrow-bullet-computer-icons-bullet-holes-angle-triangle-weapons.png" alt="" className="bluemark" />
+              <span>   Their team of GST experts has years of experience and a deep understanding of the GST laws and regulations.</span>
+              <br /><br />
+              <img src="https://w7.pngwing.com/pngs/133/147/png-transparent-blue-and-teal-arrow-logo-arrow-bullet-computer-icons-bullet-holes-angle-triangle-weapons.png" alt="" className="bluemark" />
+              <span>   They prioritize efficiency, accuracy, and exceptional customer service to help clients stay on top of their GST obligations.</span>
+              <br /><br />
+              {/* <span>Quis autem vel eum iure reprehenderit aui</span>
+              <br /> */}
+            </div>
+            {/* <button className="READMORE">
+              {" "}
+              Read More
+              <div className="container-icon"></div>
+            </button> */}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
